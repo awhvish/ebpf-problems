3b8a6d524f81cfbc481b7806e092abd3051e4c5c
Avis Lowha <high.avis.lowha@gmail.com>
2026-01-13 18:05:21 +0530

    implementation of problem-statement-2

--- problem-statement-2/main.go
+++ problem-statement-2/main.go
@@ -1,8 +1,13 @@
-package main2
+package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/rlimit"
 )
 
@@ -17,4 +22,19 @@ func main() {
 	}
 	defer objs.Close()
 
+	cgroupPath := "/sys/fs/cgroup"
+	l, err := link.AttachCgroup(link.CgroupOptions{
+		Path:    cgroupPath,
+		Program: objs.RestrictBindPort,
+		Attach:  ebpf.AttachCGroupInet4Bind,
+	})
+	if err != nil {
+		log.Fatalf("Error attaching cgroup: %s", err)
+	}
+	defer l.Close()
+	log.Printf("Successfully attached to %s. Blocking 'myprocess' on port 4040...", cgroupPath)
+
+	notify := make(chan os.Signal, 1)
+	signal.Notify(notify, os.Interrupt, syscall.SIGTERM)
+	<-notify
 }
